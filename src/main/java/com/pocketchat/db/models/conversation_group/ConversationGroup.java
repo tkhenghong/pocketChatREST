1d548463866137d1fc518c40200adb185351c9d0
Teoh Kheng Hong <tkhenghong@gmail.com>
2019-08-26 21:27:18 +0800

    Minor fixes

--- src/main/java/com/pocketchat/db/models/conversation_group/ConversationGroup.java
+++ src/main/java/com/pocketchat/db/models/conversation_group/ConversationGroup.java
@@ -4,7 +4,10 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -34,10 +37,20 @@ public class ConversationGroup {
     @NotBlank
     String description;
 
-//    @NotBlank
+    @Valid
+    @NotEmpty
+    @Size(min = 1) // If 1 of 2 persons group left, that person still can have this group
+    String[] memberIds;
+
+    @Valid
+    @NotEmpty
+    @Size(min = 1)
+    String[] adminMemberIds;
+
+    //    @NotBlank
     boolean block;
 
-//    @NotBlank
+    //    @NotBlank
     int notificationExpireDate;
 
     @NotBlank
