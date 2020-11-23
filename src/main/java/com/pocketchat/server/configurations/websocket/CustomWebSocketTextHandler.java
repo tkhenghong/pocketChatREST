1f2131fba977e76e2d76ef8669dc505ea34f6b2c
Teoh Kheng Hong <tkhenghong@gmail.com>
2020-11-23 10:27:38 +0800

    Deleted some old classes

--- src/main/java/com/pocketchat/server/configurations/websocket/CustomWebSocketTextHandler.java
+++
@@ -1,8 +0,0 @@
-package com.pocketchat.server.configurations.websocket;
-
-import org.springframework.stereotype.Component;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
-
-@Component
-public class CustomWebSocketTextHandler extends TextWebSocketHandler {
-}
